08c5404b5784201a5818676c742619b9115cc22d
doron kilzi <doronkilzi@microsoft.com>
2025-04-17 21:41:45 +0300

    move prompts to dedicated file

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,10 +3,14 @@ const {
   DRIVERS_PHOTO_TYPE,
   CONSTRUCTORS_PHOTO_TYPE,
   CURRENT_TEAM_PHOTO_TYPE,
+} = require('../constants');
+
+const {
   EXTRACT_JSON_FROM_DRIVERS_PHOTO_SYSTEM_PROMPT,
   EXTRACT_JSON_FROM_CONSTRUCTORS_PHOTO_SYSTEM_PROMPT,
   EXTRACT_JSON_FROM_CURRENT_TEAM_PHOTO_SYSTEM_PROMPT,
-} = require('../constants');
+} = require('../prompts');
+
 exports.sendLogMessage = function (bot, logMessage) {
   if (!LOG_CHANNEL_ID) {
     console.error('LOG_CHANNEL_ID is not set');
@@ -42,54 +46,54 @@ exports.mapPhotoTypeToSystemPrompt = {
 
 exports.validateJsonData = function (bot, jsonData, chatId) {
   if (!jsonData.Drivers || jsonData.Drivers.length !== 20) {
-      exports.sendLogMessage(
-          bot,
-          `Invalid JSON data. Expected 20 drivers under "Drivers" property'.`
-      );
-      bot
-        .sendMessage(
-          chatId,
-          'Invalid JSON data. Please ensure it contains 20 drivers under "Drivers" property.'
-        )
-        .catch((err) => console.error('Error sending JSON error message:', err));
-      return false;
+    exports.sendLogMessage(
+      bot,
+      `Invalid JSON data. Expected 20 drivers under "Drivers" property'.`
+    );
+    bot
+      .sendMessage(
+        chatId,
+        'Invalid JSON data. Please ensure it contains 20 drivers under "Drivers" property.'
+      )
+      .catch((err) => console.error('Error sending JSON error message:', err));
+    return false;
   }
 
   if (!jsonData.Constructors || jsonData.Constructors.length !== 10) {
-      exports.sendLogMessage(
-          bot,
-          `Invalid JSON data. Expected 10 constructors under "Constructors" property'.`
-      );
-      bot
-        .sendMessage(
-          chatId,
-          'Invalid JSON data. Please ensure it contains 10 constructors under "Constructors" property.'
-        )
-        .catch((err) => console.error('Error sending JSON error message:', err));
-      return false;
+    exports.sendLogMessage(
+      bot,
+      `Invalid JSON data. Expected 10 constructors under "Constructors" property'.`
+    );
+    bot
+      .sendMessage(
+        chatId,
+        'Invalid JSON data. Please ensure it contains 10 constructors under "Constructors" property.'
+      )
+      .catch((err) => console.error('Error sending JSON error message:', err));
+    return false;
   }
 
   if (
-      !jsonData.CurrentTeam ||
-      !jsonData.CurrentTeam.drivers ||
-      jsonData.CurrentTeam.drivers.length !== 5 ||
-      !jsonData.CurrentTeam.constructors ||
-      jsonData.CurrentTeam.constructors.length !== 2 ||
-      !jsonData.CurrentTeam.drsBoost ||
-      !jsonData.CurrentTeam.freeTransfers ||
-      !jsonData.CurrentTeam.costCapRemaining
+    !jsonData.CurrentTeam ||
+    !jsonData.CurrentTeam.drivers ||
+    jsonData.CurrentTeam.drivers.length !== 5 ||
+    !jsonData.CurrentTeam.constructors ||
+    jsonData.CurrentTeam.constructors.length !== 2 ||
+    !jsonData.CurrentTeam.drsBoost ||
+    !jsonData.CurrentTeam.freeTransfers ||
+    !jsonData.CurrentTeam.costCapRemaining
   ) {
-      exports.sendLogMessage(
-          bot,
-          `Invalid JSON data. Expected 5 drivers, 2 constructors, drsBoost, freeTransfers, and costCapRemaining properties under "CurrentTeam" property'.`
-      );
-      bot
-        .sendMessage(
-          chatId,
-          'Invalid JSON data. Please ensure it contains the required properties under "CurrentTeam" property.'
-        )
-        .catch((err) => console.error('Error sending JSON error message:', err));
-      return false;
+    exports.sendLogMessage(
+      bot,
+      `Invalid JSON data. Expected 5 drivers, 2 constructors, drsBoost, freeTransfers, and costCapRemaining properties under "CurrentTeam" property'.`
+    );
+    bot
+      .sendMessage(
+        chatId,
+        'Invalid JSON data. Please ensure it contains the required properties under "CurrentTeam" property.'
+      )
+      .catch((err) => console.error('Error sending JSON error message:', err));
+    return false;
   }
 
   return true;
