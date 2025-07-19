6681b1651b97f2a764e94cf891251c482acb63b1
doron kilzi <kilzid@gmail.com>
2025-07-19 03:04:05 +0300

    docs: document version command

--- src/constants.js
+++ src/constants.js
@@ -27,6 +27,7 @@ exports.COMMAND_GET_CURRENT_SIMULATION = '/get_current_simulation';
 exports.COMMAND_GET_BOTFATHER_COMMANDS = '/get_botfather_commands';
 exports.COMMAND_NEXT_RACE_INFO = '/next_race_info';
 exports.COMMAND_BILLING_STATS = '/billing_stats';
+exports.COMMAND_VERSION = '/version';
 exports.COMMAND_MENU = '/menu';
 
 // Menu configuration for interactive menu command
@@ -133,6 +134,11 @@ exports.MENU_CATEGORIES = {
         constant: exports.COMMAND_BILLING_STATS,
         title: '💰 Billing Stats',
         description: 'Get Azure billing statistics for the current month',
+      },
+      {
+        constant: exports.COMMAND_VERSION,
+        title: 'ℹ️ Version',
+        description: 'Show current deployed version',
       },
     ],
   },
